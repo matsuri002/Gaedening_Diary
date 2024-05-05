697de927bc44b1623a6fc289cd4929d8fbf5d4f3
matsuri002 <oimoimoimo09@gmail.com>
2024-05-05 12:31:30 +0900

    DBの操作

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Gaedening_Diary
 feature1,test1,test-branchはいらない
+pip install psycopg2
+pip install python-dotenv
--- gd.js
+++ gd.js
@@ -26,19 +26,53 @@ function goToFirstPage() {
     document.getElementById('before').style.display = 'block';
   }
   
-  function calculateDays() {
-    // 登録日の取得
-    var registrationDateInput = document.getElementById("registration-date");
-    var registrationDate = new Date(registrationDateInput.value);
+  // function calculateDays() {
+  //   // 登録日の取得
+  //   var registrationDateInput = document.getElementById("start_date");
+  //   var registrationDate = new Date(registrationDateInput.value);
   
-    // 現在の日付の取得
-    var currentDate = new Date();
+  //   // 現在の日付の取得
+  //   var currentDate = new Date();
   
-    // 日数の差を計算
-    var differenceInDays = Math.ceil((currentDate - registrationDate) / (1000 * 60 * 60 * 24));
+  //   // 日数の差を計算
+  //   var differenceInDays = Math.ceil((currentDate - registrationDate) / (1000 * 60 * 60 * 24));
   
-    // 結果を表示
-    var resultElement = document.getElementById("result");
-    resultElement.textContent = "栽培から" + differenceInDays + "日目";
-  }
+  //   // 結果を表示
+  //   var resultElement = document.getElementById("result");
+  //   resultElement.textContent = "栽培から" + differenceInDays + "日目";
+  // }
   
+  // new_entry.js
+
+
+
+  document.getElementById("vegetableForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const vegetableName = document.getElementById('vegetableName').value;
+
+    // POSTリクエストを送信して、野菜の名前をサーバーに送信する
+    fetch('/submitVegetable', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            vegetableName: vegetableName
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Success:', data);
+        alert("Vegetable name submitted successfully!");
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert("An error occurred. Please try again.");
+    });
+});
