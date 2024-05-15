22547d114af5c8e63c952f2f56266b6706035b6c
scla_arisan <oimoimoimo09@outlook.jp>
2024-05-15 18:00:07 +0900

    fix:ページ遷移の成功

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,61 +1,19 @@
-// import logo from './logo.svg';
-// import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 // App.js
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Select from './components/select.jsx';
-// import NewEntry from './components/new_entry.jsx';
-
-// const App = () => (
-//   <Router>
-//     <Routes>
-//       <Route path="/" element={<Select />} />
-//       <Route path="/NewEntry" element={<NewEntry />} />
-//     </Routes>
-//   </Router>
-// );
-
-// export default App;
-
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Select from './components/select.jsx';
 import NewEntry from './components/new_entry.jsx';
-// import Diary from './components/diary.jsx';
-// import VegetableForm from './components/new_entry.jsx';
 
-function App() {
-  return (
-    <div>
-      <Select />
-      <NewEntry />
-      {/* <TodayPage /> */}
-      {/* <VegetableForm /> */}
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Select />} />
+      <Route path="/NewEntry" element={<NewEntry />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
+
--- my-app/src/components/select.jsx
+++ my-app/src/components/select.jsx
@@ -1,12 +1,12 @@
 // select.jsx
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-// const Select = () =>(
+const Select = () =>(
 
 
-function Select() {
-  return (
+// function Select() {
+  // return (
     <div id="first">
       {/* <button onClick={goToSetupPage} id="new" className="mt-1 mb-1 btn btn-info">新規追加</button> */}
       <div id="first-1">
@@ -14,11 +14,11 @@ function Select() {
         <button>新規</button>
         {/* <div><button type="button" className="mt-1 mb-1 w-50 btn btn-primary">小松菜</button></div>
         <div><button onClick={goToTodayPage} className="mt-1 mb-1 w-50 btn btn-primary">やさい</button></div> */}
-        {/* <Link to="/screen2"><button>移動</button></Link> */}
+        <Link to="/screen2"><button>移動</button></Link>
         {/* <div id="result"></div> */}
       </div>
     </div>
   );
-}
+// }
 
 export default Select;
