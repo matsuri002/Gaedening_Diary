83bda512abaaedaba366072a1aaa94bfe5480da6
scla_arisan <oimoimoimo09@outlook.jp>
2024-05-19 00:27:56 +0900

    update:new_entry.jsx画面の修正

--- my-app/src/components/new_entry.jsx
+++ my-app/src/components/new_entry.jsx
@@ -3,20 +3,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NewEntry = () => (
-    <div id="setup">
-      <h1>新規追加</h1>
-      <form> 
-        <label htmlFor="name">名前：</label>
-        <input type="text" id="name" name="name" /><br /><br />
-        <label htmlFor="start_date">栽培開始日:</label>
-        <input type="date" id="start_date" />
-        <div id="result"></div>
-        <label htmlFor="message">メッセージ：</label><br />
-        <textarea id="message" name="message" rows="4" cols="50"></textarea><br /><br />
-        <Link to="/"><button>戻る</button></Link>
-        <input type="submit" className="ms-1 btn btn-primary" value="完了" />
-      </form>
+  <body>
+    <div className="form-container">
+      <div id="setup">
+        <h1>新規追加</h1>
+        <form> 
+          <label htmlFor="name">名前：</label>
+          <input type="text" id="name" name="name" /><br /><br />
+          <label htmlFor="start_date">栽培開始日:</label>
+          <input type="date" id="start_date" />
+          <div id="result"></div>
+          <label htmlFor="message">メッセージ：</label><br />
+          <textarea id="message" name="message" rows="4" cols="50"></textarea><br /><br />
+          <div className="form-botton-container">
+            <Link to="/"><button className="form-botton">戻る</button></Link>
+            <input type="submit" className="form-botton" value="完了" />
+          </div>
+        </form>
+      </div>
     </div>
+  </body>  
   );
 
 // )
